48fa30543a971b8dbbf9e6958b755b79c5534987
Lauan Amorim <lauan12x@gmail.com>
2023-09-01 10:48:17 -0300

    [add/error] Class Banco.cs

---
+++ AppQuinta/Banco.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppQuinta
+{
+    class Banco
+    {
+        private readonly MySqlConnection conexao = new MySqlConnection(@"Server = localhost;Database=dbAppBanco;User=root;Password=12345678");
+        private readonly MySqlCommand cmd = new MySqlCommand();
+
+        public void Open()
+        {
+            if (conexao.State == ConnectionState.Closed) 
+                conexao.Open(); 
+        }
+
+        public void Close()
+        {
+            if (conexao.State == ConnectionState.Open) 
+              conexao.Close(); 
+        }
+
+        public MySqlDataReader ExecuteReadSql(string strQuery)
+        {
+            cmd.CommandText = strQuery;
+            cmd.Connection = conexao;
+            MySqlDataReader reader = cmd.ExecuteReader();
+            return reader;
+        }
+
+    }
+}
+
--- AppQuinta/Program.cs
+++ AppQuinta/Program.cs
@@ -1,5 +1,6 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
+using System.Net ;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,44 +8,44 @@ using System.Threading.Tasks;
 
 namespace AppQuinta
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            MySqlConnection mySqlConnection = new MySqlConnection(@"Server = localhost;Database=dbAppBanco;User=root;Password=12345678");
-            mySqlConnection.Open();
-
-            Console.WriteLine("Digite o Nome: ");
-            string strNomeUsu = Console.ReadLine();
-
-            Console.WriteLine("Digite o Cargo: ");
-            string strCargo = Console.ReadLine();
-
-            Console.WriteLine("Digite a data de nascimento:");
-            string strDataNasc = Console.ReadLine();
-
-            string strInsert = string.Format("insert into tbUsuario(NomeUsu, Cargo, dataNasc)" + "values('{0}','{1}', STR_TO_DATE('{2}', '%d/%m/%Y'));", strNomeUsu, strCargo, strDataNasc);
-            MySqlCommand ObjCommand5 = new MySqlCommand(strInsert, mySqlConnection);
-            ObjCommand5.ExecuteNonQuery();
+            Banco db = new Banco();
 
 
+            //MySqlConnection mySqlConnection = new MySqlConnection(@"Server = localhost;Database=dbAppBanco;User=root;Password=12345678");
+            //mySqlConnection.Open();
 
-            string strSelect = "SELECT * FROM TBUSUARIO;";
-            MySqlCommand ObjCommand = new MySqlCommand(strSelect, mySqlConnection);
+            //Console.WriteLine("Digite o Nome: ");
+            //string strNomeUsu = Console.ReadLine();
 
-            //string strInsert = "insert into tbUsuario(NomeUsu, Cargo, dataNasc)values('Fulano','Aluno','2023/05/16'); ";
-            //MySqlCommand ObjCommand2 = new MySqlCommand(@strInsert, mySqlConnection);
-            //ObjCommand2.ExecuteNonQuery();
+            //Console.WriteLine("Digite o Cargo: ");
+            //string strCargo = Console.ReadLine();
 
-            string strDelte = "delete from tbusuario where idUsu=3; ";
-            MySqlCommand ObjCommand3 = new MySqlCommand(@strDelte, mySqlConnection);
-            ObjCommand3.ExecuteNonQuery();
+            //Console.WriteLine("Digite a data de nascimento:");
+            //string strDataNasc = Console.ReadLine();
 
-            string strUpdate = "update tbusuario set nomeusu = 'João' where IdUsu = 4;";
-            MySqlCommand ObjCommand4 = new MySqlCommand(strUpdate, mySqlConnection);
-            ObjCommand4.ExecuteNonQuery();
+            //string strInsert = string.Format("insert into tbUsuario(NomeUsu, Cargo, dataNasc)" + "values('{0}','{1}', STR_TO_DATE('{2}', '%d/%m/%Y'));", strNomeUsu, strCargo, strDataNasc);
+            //MySqlCommand ObjCommand5 = new MySqlCommand(strInsert, mySqlConnection);
+            //ObjCommand5.ExecuteNonQuery();
 
-            MySqlDataReader reader = ObjCommand.ExecuteReader();
+            ////string strInsert = "insert into tbUsuario(NomeUsu, Cargo, dataNasc)values('Fulano','Aluno','2023/05/16'); ";
+            ////MySqlCommand ObjCommand2 = new MySqlCommand(@strInsert, mySqlConnection);
+            ////ObjCommand2.ExecuteNonQuery();
+
+            //string strDelte = "delete from tbusuario where idUsu=3; ";
+            //MySqlCommand ObjCommand3 = new MySqlCommand(@strDelte, mySqlConnection);
+            //ObjCommand3.ExecuteNonQuery();
+
+            //string strUpdate = "update tbusuario set nomeusu = 'João' where IdUsu = 4;";
+            //MySqlCommand ObjCommand4 = new MySqlCommand(strUpdate, mySqlConnection);
+            //ObjCommand4.ExecuteNonQuery();
+
+            string strSelect = "SELECT * FROM tbUsuario;";
+
+            MySqlDataReader reader = db.ExecuteReadSql(strSelect);
 
             while (reader.Read()) {
                 Console.WriteLine("Código = {0} |   Nome = {1}  | Cargo = {2}   | Nascimento = {3}",
