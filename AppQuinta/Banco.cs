88c8acba4dd26722e2d9f16f77b8e0748268e23f
MatheusGamaRussi <matheus.russi@etec.sp.gov.br>
2023-09-11 10:43:12 -0300

    [fix~]

--- AppQuinta/Banco.cs
+++ AppQuinta/Banco.cs
@@ -1,17 +1,13 @@
 ﻿using MySql.Data.MySqlClient;
-using System.Data;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Configuration;
+using System.Data;
 
 namespace AppQuinta
 {
     class Banco
     {
-        private readonly MySqlConnection conexao = new MySqlConnection(ConfigurationManager.ConnectionStrings["Conexao"].ConnectionString);
+        private readonly MySqlConnection conexao = new MySqlConnection(ConfigurationManager.ConnectionStrings["conexao"].ConnectionString);
         private readonly MySqlCommand cmd = new MySqlCommand();
 
         public void Open()
@@ -20,11 +16,6 @@ namespace AppQuinta
                 conexao.Open(); 
         }
 
-        public void Close()
-        {
-            if (conexao.State == ConnectionState.Open) 
-                conexao.Close(); 
-        }
 
         public MySqlDataReader ExecuteReadSql(string strQuery)
         {
@@ -36,8 +27,8 @@ namespace AppQuinta
 
         public void ExecuteNowdSql(string strQuery)
         {
-            cmd.CommandText = strQuery;
             cmd.Connection = conexao;
+            cmd.CommandText = strQuery;
             cmd.ExecuteNonQuery();
         }
 
@@ -48,7 +39,11 @@ namespace AppQuinta
             string strRetorno = Convert.ToString(cmd.ExecuteScalar());
             return strRetorno;
         }
-
+        public void Close()
+        {
+            if (conexao.State == ConnectionState.Open) 
+                conexao.Close(); 
+        }
 
     }
 }
--- AppQuinta/Program.cs
+++ AppQuinta/Program.cs
@@ -15,9 +15,7 @@ namespace AppQuinta
         {
             Banco db = new Banco();
 
-             MySqlConnection Conexao = new MySqlConnection(ConfigurationManager.ConnectionStrings["Conexao"].ConnectionString);
-             Conexao.Open();
-
+            db.Open();
             Console.WriteLine("Digite o Nome: ");
             string strNomeUsu = Console.ReadLine();
 
@@ -26,29 +24,10 @@ namespace AppQuinta
 
             Console.WriteLine("Digite a data de nascimento:");
             string strDataNasc = Console.ReadLine();
-
-            string strInsert = string.Format("insert into tbUsuario(NomeUsu, Cargo, dataNasc)" +
-                "values('{0}','{1}', " +
-                "STR_TO_DATE('{2}', '%d/%m/%Y'));", strNomeUsu, strCargo, strDataNasc);
-            // MySqlCommand ObjCommandI = new MySqlCommand(strInsert, Conexao);
-            db.ExecuteNowdSql(strInsert);
-
-            //string strInsert = "insert into tbUsuario(NomeUsu, Cargo, dataNasc)values('Fulano','Aluno','2023/05/16'); ";
-            //MySqlCommand ObjCommand2 = new MySqlCommand(@strInsert, mySqlConnection);
-            //ObjCommand2.ExecuteNonQuery();
-            db.Open();
-            string strUpdate = "update tbusuario set nomeusu = 'João' where IdUsu = 4;";
-            //MySqlCommand ObjCommandUP = new MySqlCommand(strUpdate, Conexao);
-            db.ExecuteNowdSql(strUpdate);
-
-            string strDelete = "delete from tbusuario where idUsu=3; ";
-            //MySqlCommand ObjCommandD = new MySqlCommand(@strDelete, Conexao);
-            db.ExecuteNowdSql(strDelete);
-
-            db.Open();
-            string strSelect = "SELECT * FROM tbUsuario;";
             
+            string strSelect = "SELECT * FROM tbUsuario;";
             MySqlDataReader reader = db.ExecuteReadSql(strSelect);
+
 
 
             while (reader.Read()) {
@@ -56,10 +35,28 @@ namespace AppQuinta
                     reader["IdUsu"], reader["NomeUsu"],reader["Cargo"],reader["DataNasc"]);
             }
 
-            
             reader.Close();
-            db.Open();
+
+            string strInsert = string.Format("insert into tbUsuario(NomeUsu, Cargo, dataNasc)" +
+                "values('{0}','{1}', " +
+                "STR_TO_DATE('{2}', '%d/%m/%Y'));", strNomeUsu, strCargo, strDataNasc);;
+            db.ExecuteNowdSql(strInsert);
+
+            string strUpdate = "update tbusuario set nomeusu = 'João' where IdUsu = 4;";
+            db.ExecuteNowdSql(strUpdate);
+
+            string strDelete = "delete from tbusuario where idUsu=3; ";
+            db.ExecuteNowdSql(strDelete);
+            Console.WriteLine("Informe o Id para identificação: ");
+            string IdUsu = Console.ReadLine();
+
+            string strSelectId = "SELECT NOMEUSU FROM TBUSUARIO WHERE IDUSU=" + IdUsu + ";";
+
+            string strDado = db.ExecuteScalarSql(strSelectId);
+            Console.WriteLine("Olá senhor(a) " + strDado);
             db.Close();
+
+            db.Open();
             Console.ReadLine();
             //Console.WriteLine("Banco conectado!");
 
