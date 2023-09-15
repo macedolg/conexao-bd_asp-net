faae6eb2f68371c279707455f51045582a543c4f
lauan12x@gmail.com <matheus.russi@etec.sp.gov.br>
2023-09-15 11:29:19 -0300

    [Add] UsuarioDAO

--- AppQuinta/Program.cs
+++ AppQuinta/Program.cs
@@ -1,11 +1,4 @@
-﻿using MySql.Data.MySqlClient;
-using System;
-using System.Net ;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Configuration;
+﻿using System;
 
 namespace AppQuinta
 {
@@ -13,9 +6,17 @@ namespace AppQuinta
     {
         static void Main(string[] args)
         {
-            Banco db = new Banco();
 
-            db.Open();
+            UsuarioDAO objDAO = new UsuarioDAO();
+            
+
+
+
+            Console.WriteLine("Informe o Id para identificação: ");
+            string strIdUsu = Console.ReadLine();
+            string strDado = objDAO.SelectDado(strIdUsu);
+            Console.WriteLine("Olá senhor(a) " + strDado);
+
             Console.WriteLine("Digite o Nome: ");
             string strNomeUsu = Console.ReadLine();
 
@@ -24,40 +25,24 @@ namespace AppQuinta
 
             Console.WriteLine("Digite a data de nascimento:");
             string strDataNasc = Console.ReadLine();
-            
-            string strSelect = "SELECT * FROM tbUsuario;";
-            MySqlDataReader reader = db.ExecuteReadSql(strSelect);
+
+            objDAO.Insert(strNomeUsu, strCargo, strDataNasc);
+
+            objDAO.SelectDado(strIdUsu);     
+
+            objDAO.Select();
 
 
 
-            while (reader.Read()) {
-                Console.WriteLine("Código = {0} |   Nome = {1}  | Cargo = {2}   | Nascimento = {3}",
-                    reader["IdUsu"], reader["NomeUsu"],reader["Cargo"],reader["DataNasc"]);
-            }
+            //objDAO.Update(strNomeUsu, strCargo, strDataNasc, strIdUsu);
 
-            reader.Close();
+            //objDAO.Delete(strIdUsu);
 
-            string strInsert = string.Format("insert into tbUsuario(NomeUsu, Cargo, dataNasc)" +
-                "values('{0}','{1}', " +
-                "STR_TO_DATE('{2}', '%d/%m/%Y'));", strNomeUsu, strCargo, strDataNasc);;
-            db.ExecuteNowdSql(strInsert);
 
-            string strUpdate = "update tbusuario set nomeusu = 'João' where IdUsu = 4;";
-            db.ExecuteNowdSql(strUpdate);
 
-            string strDelete = "delete from tbusuario where idUsu=3; ";
-            db.ExecuteNowdSql(strDelete);
-            Console.WriteLine("Informe o Id para identificação: ");
-            string IdUsu = Console.ReadLine();
+            //string strSelectId = "SELECT NOMEUSU FROM TBUSUARIO WHERE IDUSU=" + 
+            //    strIdUsu + ";";
 
-            string strSelectId = "SELECT NOMEUSU FROM TBUSUARIO WHERE IDUSU=" + IdUsu + ";";
-
-            string strDado = db.ExecuteScalarSql(strSelectId);
-            Console.WriteLine("Olá senhor(a) " + strDado);
-            db.Close();
-
-            db.Open();
-            Console.ReadLine();
             //Console.WriteLine("Banco conectado!");
 
         }
---
+++ AppQuinta/UsuarioDAO.cs
@@ -0,0 +1,79 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppQuinta
+{
+     class UsuarioDAO
+    {
+        Banco db = new Banco();
+
+        public void Insert(string strNomeUsu, string strCargo, string strDataNasc)
+        {
+            string strInsert = string.Format("insert into tbUsuario(NomeUsu, Cargo, dataNasc)" +
+            "values('{0}','{1}', " +
+            "STR_TO_DATE('{2}', '%d/%m/%Y'));", 
+            strNomeUsu, strCargo, strDataNasc);
+
+            db.Open();
+            db.ExecuteNowdSql(strInsert);
+            db.Close();
+        }
+
+        public void Update(string strNomeUsu, string strCargo, string strDataNasc, string strIdUsu)
+        {
+            string strUpdate = string.Format("update tbUsuario set NomeUsu = '{0}'," +
+                "Cargo = '{1}', DataNasc = STR_TO_DATE('{2}', '%d/%m/%Y') " +
+                "where idUsu = '{3}';", 
+                strNomeUsu, strCargo, strDataNasc, strIdUsu);
+            db.Open();
+            db.ExecuteNowdSql(strUpdate);
+            db.Close();
+        }
+
+        public void Delete(string strIdUsu)
+        {
+            string strDelete = string.Format("delete from tbusuario where idUsu= {0};",
+                strIdUsu);
+
+            db.Open();
+            db.ExecuteNowdSql(strDelete);
+            db.Close();
+        }
+
+        public string SelectDado(string strIdUsu)
+        {
+            string strDado = "select NomeUsu from tbusuario where IdUsu = " +
+                strIdUsu +
+                ";";
+
+            db.Open();
+            strDado = db.ExecuteScalarSql(strDado);
+            db.Close();
+
+            return strDado;
+        }
+
+        public MySqlDataReader Select()
+        {
+            string strSelect = "select * from tbusuario;";
+
+            db.Open();
+            MySqlDataReader reader = db.ExecuteReadSql(strSelect);
+            while (reader.Read())
+            {
+                Console.WriteLine("Código = {0} |   Nome = {1}  | Cargo = {2}   | Nascimento = {3}",
+                     reader["IdUsu"], reader["NomeUsu"], reader["Cargo"], reader["DataNasc"]);
+            }
+            Console.ReadLine();
+
+            reader.Close();
+            db.Close();
+            return reader;
+
+        }
+    }
+}
